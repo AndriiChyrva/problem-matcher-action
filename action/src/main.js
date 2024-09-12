b9a4c8392f420826514c3452c714ed0277dbd3f9
Andrii Chyrva <s-achyrva@aras.com>
2024-09-12 14:13:31 +0300

    v1.0.1

--- action/src/main.js
+++ action/src/main.js
@@ -8,95 +8,12 @@ const path = require('path')
  */
 async function run() {
   try {
-    console.log('Bankai!!!')
-
-    // core.debug	             debug
-    // core.notice	           notice
-    // core.warning	           warning
-    // core.error	             error
-    // core.startGroup	       group
-    // core.endGroup	         endgroup
-    // core.setCommandEcho	   echo
-    // core.setSecret	         add-mask
-
-    // console.log('Debug')
+    console.log('Add-Matcher')
     command.issueCommand(
-      'debug',
-      { message: 'Some Debug message here' },
-      'Some Debug message here'
+      'add-matcher',
+      {},
+      path.join(__dirname, 'problem-matcher.json')
     )
-
-    // console.log('Notice')
-    command.issueCommand(
-      'notice',
-      { message: 'Some Notice message here' },
-      'Some Notice message here'
-    )
-
-    // console.log('Warning')
-    command.issueCommand(
-      'warning',
-      { message: 'Some Warning message here' },
-      'Some Warning message here'
-    )
-
-    // console.log('Error')
-    command.issueCommand(
-      'error',
-      { message: 'Some Error message here' },
-      'Some Error message here'
-    )
-
-    // console.log('Echo')
-    command.issueCommand('echo', { enabled: true }, 'on')
-    command.issueCommand('echo', { enabled: false }, 'off')
-
-    command.issueCommand(
-      'setCommandEcho',
-      { enabled: true },
-      'Some Echo message here'
-    )
-    command.issueCommand(
-      'setCommandEcho',
-      { enabled: false },
-      'Some Echo message here'
-    )
-
-    // console.log('Group')
-    command.issueCommand(
-      'group',
-      { name: 'Some Group name here' },
-      'Some Group name here'
-    )
-    console.log('Inside the Group')
-
-    // console.log('Endgroup')
-    command.issueCommand(
-      'endgroup',
-      { name: 'Some Endgroup name' },
-      'Some EndGroup name here'
-    )
-
-    // console.log('add-mask')
-    command.issueCommand(
-      'add-mask',
-      { secret: 'Some Secret is here' },
-      'Some Secret is here'
-    )
-
-    // console.log('set-output')
-    command.issueCommand(
-      'setOutput',
-      { name: 'name', value: 'value' },
-      'setOutput'
-    )
-
-    // console.log('Add-Matcher')
-    // command.issueCommand(
-    //   'add-matcher',
-    //   {},
-    //   path.join(__dirname, 'problem-matcher.json')
-    // )
   } catch (error) {
     // Fail the workflow run if an error occurs
     core.setFailed(error.message)
